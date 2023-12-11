dcbd47aa4d97ca9790d0ce0319c4c82b81ac7489
Oli Bridge <ob344@cam.ac.uk>
2023-12-11 15:47:35 +0000

    Day 11 completed

--- 2023/11-Dec.py
+++ 2023/11-Dec.py
@@ -1,38 +1,30 @@
 import numpy as np
 
-arry = np.array([[1,2,3],[4,5,6],[7,8,9]])
-print(arry)
-print(np.insert(arry,1,[[10,11,12]],axis=1))
+def empty_indices(data: np.ndarray) -> list:
+    """Get indices of empty rows in 2D array."""
+    return [i for i, row in enumerate(data) if np.all(row == '.')]
 
-with open('data/test.txt', 'r') as f:
+def galaxy_dists(data: np.ndarray, n: int) -> int:
+    """Get sum of distances between galaxies given space expansion multiplier."""
+    empty_rows = empty_indices(data)
+    empty_cols = empty_indices(data.T)
+    gals = [[i,j] for i, j in np.ndindex(data.shape) if data[i,j] == '#']
+
+    dists = []
+    for i, gal1 in enumerate(gals):
+        for gal2 in gals[i+1:]:
+            xlims = sorted([gal1[0], gal2[0]])
+            ylims = sorted([gal1[1], gal2[1]])
+
+            n_empty_rows = len([k for k in empty_rows if xlims[0] < k < xlims[1]])
+            n_empty_cols = len([k for k in empty_cols if ylims[0] < k < ylims[1]])
+
+            dists.append(xlims[1] - xlims[0] + ylims[1] - ylims[0] + (n - 1) * (n_empty_rows + n_empty_cols))
+
+    return sum(dists)
+
+with open('data/11-Dec.txt', 'r') as f:
     data = np.array([list(line.strip()) for line in f.readlines()])
 
-print(data)
-
-added_rows = 0
-for i, row in enumerate(data):
-    if np.all(row == '.'):
-        added_rows += 1
-        data = np.insert(data, i + added_rows, [np.full(len(row), '.')], axis=0)
-
-added_cols = 0
-for i, col in enumerate(data.T):
-    if np.all(col == '.'):
-        added_cols += 1
-        data = np.insert(data, i + added_cols, [np.full(len(col), '.')], axis=1)
-
-print(data)
-gals = []
-for i, row in enumerate(data):
-    for j, _ in enumerate(row):
-        if data[i,j] == '#':
-            gals.append([i,j])
-
-print(gals)
-
-dists = []
-for i, gal in enumerate(gals):
-    for j, gal2 in enumerate(gals[i+1:]):
-        dists.append(np.abs(gal[0] - gal2[0]) + np.abs(gal[1] - gal2[1]))
-
-print(sum(dists))
+print(f'Part 1: {galaxy_dists(data, 2)}')
+print(f'Part 2: {galaxy_dists(data, 1000000)}')
