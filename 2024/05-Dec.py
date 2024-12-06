09328cbad2bb2241574c94e04f678bc829ef9382
Oli Bridge <ob344@cam.ac.uk>
2024-12-06 15:52:19 +0000

    2024 Day 6

--- 2024/05-Dec.py
+++ 2024/05-Dec.py
@@ -1,56 +1,47 @@
 from collections import defaultdict
+from typing import List, Dict
+
+def split_updates(rules: Dict[str, List[str]], updates: List[List[str]]) -> List[List[str]]:
+    valid = []
+    invalid = []
+    for u in updates:
+        is_valid = True
+        for i, n in enumerate(u):
+            history = u[:i]
+            if any([h in rules[n] for h in history]):
+                is_valid = False
+                invalid.append(u)
+                break
+        if is_valid:
+            valid.append(u)
+    return valid, invalid
+
+def reorder_invalid(rules: Dict[str, List[str]], invalid: List[List[str]]) -> List[List[str]]:
+    invalid_modified = []
+    for u in invalid:
+        for i, n in enumerate(u):
+            history = u[:i]
+            indices = [i for i, h in enumerate(history) if h in rules[n]]
+            if indices:
+                earliest = min(indices)
+                u = u[:earliest] + [n] + u[earliest:i] + u[i+1:] # Put n before earliest violation of rules
+
+        invalid_modified.append(u)
+    return invalid_modified
 
 with open('data/05-Dec.txt') as f:
-    # Split into two variables by the linebreak
     data = f.read().split('\n\n')
     rule_list = data[0].splitlines()
     updates = [n.split(',') for n in data[1].splitlines()]
 
+# Create dict of rules
 rules = defaultdict(list)
 for r in rule_list:
     r = r.split('|')
     rules[r[0]].append(r[1])
 
-# Loop to print rules
-for k, v in rules.items():
-    print(k, v)
+valid, invalid = split_updates(rules, updates)
+invalid_modified = reorder_invalid(rules, invalid)
 
-for u in updates:
-    print(u)
-
-valid = []
-invalid = []
-for u in updates:
-    is_valid = True
-    for i, n in enumerate(u):
-        history = u[:i]
-        if any([h in rules[n] for h in history]):
-            is_valid = False
-            invalid.append(u)
-            break
-    if is_valid:
-        valid.append(u)
-
-# Print sum of middle characters of valid
-print(sum([int(v[len(v)//2]) for v in valid]))
-
-print(valid)
-print(invalid)
-
-invalid_modified = []
-# For each invalid update, reorder the numbers to satisfy the rules
-for u in invalid:
-    for i, n in enumerate(u):
-        history = u[:i]
-        # Find indices of all of h in the rules
-        indices = [i for i, h in enumerate(history) if h in rules[n]]
-        if indices:
-            # Find indices of earliest occurence of h in the rules
-            earliest = min(indices)
-            # Put n in the earliest position and shift the rest forward
-            u = u[:earliest] + [n] + u[earliest:i] + u[i+1:]
-
-    invalid_modified.append(u)
-
-print(invalid_modified)
-print(sum([int(v[len(v)//2]) for v in invalid_modified]))
+print(f'Part 1: {sum([int(v[len(v)//2]) for v in valid])}')
+print(f'Part 2: {sum([int(v[len(v)//2]) for v in invalid_modified])}')
---
+++ 2024/06-Dec.py
@@ -0,0 +1,54 @@
+import numpy as np
+from itertools import cycle
+
+def simulate(pos: np.array, data: np.array):
+    dirs = cycle([np.array([-1, 0]), np.array([0, 1]), np.array([1, 0]), np.array([0, -1])])
+    dir = next(dirs)
+    
+    visited = {tuple(pos)}
+    dir_cache = {(pos[0], pos[1], dir[0], dir[1])}
+
+    while True:
+        new_pos = tuple(pos + dir) # Get next position
+    
+        if new_pos[0] < 0 or new_pos[0] >= data.shape[0] or new_pos[1] < 0 or new_pos[1] >= data.shape[1]:
+            return visited, False
+        
+        elif data[new_pos] == '#':
+            dir = next(dirs)
+
+        else:
+            pos = np.array(new_pos)
+            visited.add(tuple(pos))
+
+            # Check if np array dirs[dir_i] is in visited
+            c = (pos[0], pos[1], dir[0], dir[1])
+            if c in dir_cache:
+                return visited, True
+            else:
+                dir_cache.add(c)
+
+with open('data/06-Dec.txt') as f:
+    data = np.array([list(line.strip()) for line in f.readlines()])
+    
+pos = np.array(np.where(data == '^')).T[0]
+visited, loop = simulate(pos, data) # Part 1, simulate on the original data
+
+count = 0
+candidates = visited - set([tuple(pos)]) # Part 2, check for infinite loops after adding obstacle to each visited position except the starting one
+
+for i in range(data.shape[0]):
+    for j in range(data.shape[1]):
+
+        if (i, j) in candidates:
+            old_char = data[i, j]
+            data[i, j] = '#'
+
+            _, loop = simulate(pos, data)
+            if loop:
+                count += 1
+
+            data[i, j] = old_char
+    
+print(f'Part 1: {len(visited)}')
+print(f'Part 2: {count}')
